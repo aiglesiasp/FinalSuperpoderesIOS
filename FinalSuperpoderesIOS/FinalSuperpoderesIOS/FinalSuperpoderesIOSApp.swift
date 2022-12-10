8481ec747a23d9ad00c29963fbe486a0c5619e7a
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-10 12:55:12 +0100

    Inicio Proyecto

---
+++ FinalSuperpoderesIOS/FinalSuperpoderesIOS/FinalSuperpoderesIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalSuperpoderesIOSApp.swift
+//  FinalSuperpoderesIOS
+//
+//  Created by Aitor Iglesias Pubill on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalSuperpoderesIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
