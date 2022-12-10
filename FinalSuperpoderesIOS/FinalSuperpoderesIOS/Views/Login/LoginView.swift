70b1213f17ceac6f2cece7664ff27543520e71f2
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-10 15:03:28 +0100

    View Login finish

--- FinalSuperpoderesIOS/FinalSuperpoderesIOS/Views/Login/LoginView.swift
+++ FinalSuperpoderesIOS/FinalSuperpoderesIOS/Views/Login/LoginView.swift
@@ -7,12 +7,60 @@
 
 import SwiftUI
 
+
+
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //MARK: Imagen FONDO
+            //Imagen de FONDO Marvel
+            Image(decorative: "marvelFondo")
+                .resizable()
+                .opacity(1)
+            //Imagen de FONDO Negra
+            Image(decorative: "")
+                .resizable()
+                .background(.black)
+                .opacity(0.3)
+            
+            //MARK: TITULO
+            //TEXTO
+            VStack {
+                //TEXTO
+                Image(decorative: "marvelTitle")
+                    .resizable()
+                    .scaledToFit() //escalar
+                    .frame(width: 300)
+                    .opacity(0.4)
+                    .padding(.top, 150)
+                Spacer()
+                //BOTON PARA ENTRAR
+                Button {
+                    //TODO: Pasar a la vista de HOME
+                } label: {
+                    Text("ENTRAR")
+                        .padding()
+                        .font(.custom("", size: 30))
+                        .bold()
+                        .foregroundColor(.black)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(.white.opacity(0.9))
+                        .cornerRadius(20)
+                        .shadow(radius: 1.0, x:10, y:10)
+                }
+                Spacer()
+
+                }
+            }
+        .ignoresSafeArea()
+        
     }
 }
 
+
+
+//MARK: PREVIEW
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
